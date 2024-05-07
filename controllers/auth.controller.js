0ee79a67e6d452551cbb8ed54376724343a107ce
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2024-05-07 16:22:54 +0700

    hotfix: vietnamese username

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -193,8 +193,8 @@ const handleGoogleLogin = async (req, res) => {
         let user = await User.findOne({ email });
         if (!user) {
             // If user doesn't exist, create a new user
-            const username = name.replace(/\s/g, "") + uuidv4().substring(0, 5);
-            const password = uuidv4();
+            const username = normalizeVietnamese(name) + uuidv4().substring(0, 5);
+            const password = uuidv4().substring(0, 6);;
 
             // Hash the generated password
             const salt = await bcrypt.genSalt(10);
@@ -262,5 +262,27 @@ const handleGoogleLogin = async (req, res) => {
         res.status(500).json("Error login with Google");
     }
 };
+
+function normalizeVietnamese(str) {
+    str = str.replace(/A|Á|À|Ã|Ạ|Â|Ấ|Ầ|Ẫ|Ậ|Ă|Ắ|Ằ|Ẵ|Ặ/g, "A");
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/E|É|È|Ẽ|Ẹ|Ê|Ế|Ề|Ễ|Ệ/, "E");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/I|Í|Ì|Ĩ|Ị/g, "I");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/O|Ó|Ò|Õ|Ọ|Ô|Ố|Ồ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ỡ|Ợ/g, "O");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/U|Ú|Ù|Ũ|Ụ|Ư|Ứ|Ừ|Ữ|Ự/g, "U");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/Y|Ý|Ỳ|Ỹ|Ỵ/g, "Y");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/Đ/g, "D");
+    str = str.replace(/đ/g, "d");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, "");
+    str = str.replace(/\u02C6|\u0306|\u031B/g, "");
+    str = str.replace(/\s/g, "")
+    return str;
+}
 
 module.exports = { handleLogin, handleForget, handleRecover, handleVerifyToken, handleGoogleLogin };
